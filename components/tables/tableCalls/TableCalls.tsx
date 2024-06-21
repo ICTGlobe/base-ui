674c9a4f2479a5ff46ceae6b8556b124023bbc15
Craig <ch@ictlabs.io>
2024-06-21 11:33:25 +0200

    feat/add data table

--- components/tables/tableCalls/TableCalls.tsx
+++ components/tables/tableCalls/TableCalls.tsx
@@ -1,8 +1,8 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DataTable } from "@/components/ui/data-table";
 import { Table } from "@/components/ui/table";
-import {columns} from "./columns"
-import data from "@/data/calls_data.json"
+import { columns } from "./columns";
+import data from "@/data/calls_data.json";
 
 const TableCalls = () => {
   return (
